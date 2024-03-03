302c9c372a41ddcac27c29f549dd6f64155aa098
SantiagoMelo0104 <Santiago.melo0104@gmail.com>
2024-03-03 17:53:28 -0500

    Calculadora funcional

--- src/main/java/org/arep/SparkWebServer.java
+++ src/main/java/org/arep/SparkWebServer.java
@@ -8,19 +8,30 @@ public class SparkWebServer {
     public static void main(String... args){
         staticFiles.location("/public");
         port(getPort());
-        get("Sin", (req,res) -> {
+        get("/Sin", (req,res) -> {
             String number= req.queryParams("number");
             return Math.sin(Double.parseDouble(number));
         });
-        get("/cos", (req,res) -> {
+        get("/Cos", (req,res) -> {
             String number= req.queryParams("number");
             System.out.println("-------------" + number);
             return "" + Math.cos(Double.parseDouble(number));
         });
-//        get("Palindromo", (req,res) -> {
-//            String palabra= req.queryParams("palabra");
-//            return formulario(palabra);
-//        });
+        get("/Palindromo", (req,res) -> {
+            String word= req.queryParams("word").toLowerCase();
+            String wordInvert= invertWord(word);
+            if(word.equals(wordInvert)){
+                return "La palabra " + word + " es palíndroma 😊😊😊";
+            }else {
+                return "La palabra " + word + " NO es palíndroma 😒😒😒 ";
+            }
+        });
+        get("/Magnitud", (req,res) -> {
+            Double number1= Double.valueOf((req.queryParams("number1")));
+            Double number2= Double.valueOf(req.queryParams("number2"));
+            Double result= Math.sqrt(pow(number1,2) + pow(number2,2));
+            return "" + result.toString();
+        });
 
     }
 
@@ -29,6 +40,16 @@ public class SparkWebServer {
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 4567;
+    }
+
+    private static String invertWord(String word) {
+        String invertWord ="";
+        char caracter;
+        for (int i=word.length()-1; i>=0; i--) {
+            caracter = word.charAt(i);
+            invertWord += caracter;
+        }
+        return invertWord;
     }
 
 
